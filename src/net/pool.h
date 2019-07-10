baf864dce9d2e2885cf1952064f78cf56a84bc8a
Spatocode <impulseofficial069@gmail.com>
2019-07-10 17:47:16 +0100

    pool structure

---
+++ src/net/pool.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Port {
+	int difficulty;
+	char host[20];
+	int port;
+	int maxconn;
+};
+
+struct Stratum {
+	char timeout[20];
+	struct Port ports;
+};
+
+struct Config {
+	char address[20];
+	struct Stratum stratum;
+	bool bypassAddressValidation;
+	bool bypassShareValidation;
+	char blockRefreshInterval[20];
+	char upstreamCheckInterval[20];
+	char estimationWindow[20];
+	char luckWindow[20];
+	char largeLuckWindow[20];
+	int threads;
+	char newrelicKey[20];
+	char newrelicName[20];
+	bool newrelicEnabled;
+	bool newrelicVerbose;
+};
+
+
+
+
+
+
+
+
+
+
