c028119e39ebfab71e5387875fb95f366658de6b
Spatocode <impulseofficial069@gmail.com>
2019-07-16 23:21:45 +0100

    create socket for server

--- net/endpoint.h
+++ net/endpoint.h
@@ -10,16 +10,17 @@ namespace minercave {
 
 class EndPoint {
 	public:
-		EndPoint(Port cfg);
+		EndPoint(Pool::Port cfg);
+		~EndPoint();
 		void listen();
 		
 	private:
-		unsigned int jobSequence;
-		Pool::Port config;
-		short instanceId[];
-		unsigned int extraNonce;
-		std::string targetHex;
-		int difficulty;
+		unsigned int m_jobSequence;
+		Pool::Port m_config;
+		short m_instanceId[4];
+		unsigned int m_extraNonce;
+		std::string m_targetHex;
+		int m_difficulty;
 };
 
 
